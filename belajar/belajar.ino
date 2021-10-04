29bead2530efd3b3157029770c0a74e633d316e5
dzif3 <60211843+dzif3@users.noreply.github.com>
2021-10-04 20:12:46 +0700

    Add files via upload

---
+++ belajar/belajar.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // *semua program di void setup hanya akan di jalankan sekali saja*/
+  Serial.begin(9600);
+  Serial.println("welcome to Idn Broadind school");
+
+}
+
+void loop() {
+  // *semua program yang ada di void loop() akan di jalankan berkali - kali secara berkala*/
+
+  Serial.println("welcome to idn");
+  delay(1000);
+
+}
